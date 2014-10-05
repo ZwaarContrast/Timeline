797d9822bc24e3e5ebd767383eb4f14b15af02f5
Danny Hoek <danny@zwaarcontrast.nl>
2014-10-05 17:26:29 +0200

    Changed tweening of handle

--- js/timelineline.js
+++ js/timelineline.js
@@ -189,7 +189,12 @@
 			//_self.handle.style.width = _self.isFullscreen ? _self.config.slideshowRatio * 100*_self.slidesCount + '%' : _self.slidesCount * 100 + '%';
 
 			var handleWidth = _self.isFullscreen ? _self.config.slideshowRatio * 100*_self.slidesCount + '%' : _self.slidesCount * 100 + '%';
-			TweenLite.to(_self.handle, 0, {width:handleWidth});
+			
+			if(_self.isFullscreen){
+				TweenLite.to(_self.handle, 0.4, {width:handleWidth});
+			}else{
+				TweenLite.to(_self.handle, 0.4, {width:handleWidth});
+			}
 			
 
 			//Calculate distance for translate
